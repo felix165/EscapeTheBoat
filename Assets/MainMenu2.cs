a592630a7b11332ca993b141e986849178818e4b
filbertfl <filbertlimm@gmail.com>
2023-04-17 22:27:26 +0700

    Update MainMenu2.cs

--- Assets/MainMenu2.cs
+++ Assets/MainMenu2.cs
@@ -9,4 +9,9 @@ public class MainMenu2 : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
