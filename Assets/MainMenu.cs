441e624b24b3ce4c5797656a0b9b519bdb6b4d5d
filbertfl <filbertlimm@gmail.com>
2023-04-17 22:18:09 +0700

    main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,6 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
+<<<<<<< Updated upstream
 
     
+=======
+>>>>>>> Stashed changes
 }
